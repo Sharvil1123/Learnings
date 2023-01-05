a3514830a7ef882fb2edf1f145af723ca3deea85
Sharvil1123 <101034297+Sharvil1123@users.noreply.github.com>
2023-01-05 20:11:14 +0530

    Module exporting

---
+++ moduleExporting.js
@@ -0,0 +1,18 @@
+// import playGuitar from "./guitars";
+// import { shredding as shred, plucking as pluck }  from "./guitars";
+
+// console.log(playGuitar());
+// console.log(shred());
+// console.log(pluck());
+
+// import * as Guitars from "./guitars.js";
+
+
+// console.log(Guitars.playGuitar());
+// console.log(Guitars.shredding());
+// console.log(Guitars.plucking());
+
+
+// import * as newUsers from "./user";
+// const me  = new newUsers("sharvilbhalke@gmail.com", "Sharvil");
+// console.log(me);
