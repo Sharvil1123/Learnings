50bed2eb876b7d967f28565abc2200e79e5668cc
Sharvil1123 <101034297+Sharvil1123@users.noreply.github.com>
2023-01-08 18:00:07 +0530

    Switch Statements

---
+++ switchStatements.js
@@ -0,0 +1,39 @@
+/* SWITCH STATEMENTS
+switch(expression or value){
+    case choice1:
+        //code
+        break;
+
+    case choice2:
+        //code
+        break;
+    
+    // add as many cases as you want
+
+    default:
+        //run this code if no case matches
+        // no break needed
+}
+*/
+
+// Example----->
+/*
+switch(Math.floor(Math.random() * 5 + 1)){
+    case 1:
+        console.log(1);
+        break;
+
+    case 2:
+        console.log(2);
+        break;
+
+    case 3:
+        console.log(3);
+        break;
+
+    default:
+        console.log("No match Found");
+
+    
+}
+*/
